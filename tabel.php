b72591ee4eb94d193d6c12eda398dd97464a25e0
Aisyah Muthmainnah <64567761+aisyahmuthmainnah@users.noreply.github.com>
2020-06-09 22:54:12 +0700

    Add files via upload

---
+++ tabel.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="tabel.css" media="screen">
+    <link rel="stylesheet" href="bootstrap.css" media="screen">
+    <link rel="stylesheet" href="../_assets/css/custom.min.css">
+</head>
+<body>
+<div id="outtable">
+  <table>
+  	
+  	<tbody>
+  		<tr>
+  			<td>
+			<div class="card border-primary mb-3" style="max-width: 10rem;">
+  <div class="card-header" align='right'><button type="button" class="btn btn-danger btn-sm"">Penuh</button></div>
+  <div class="card-body">
+    <h6 class="card-title">PASAR AYAM</h6>
+    <p class="card-text"></p>
+  </div>
+  </td>
+  			<td width='50px'></td>
+  			<td>	<div class="card border-primary mb-3" style="max-width: 10rem;">
+  <div class="card-header" align='right'><button type="button" class="btn btn-danger btn-sm"">Penuh</button></div>
+  <div class="card-body">
+    <h6 class="card-title">PASAR DAGING</h6>
+    <p class="card-text"></p>
+  </div></td>
+  			<td width='50px'></td>
+  			<td width='50px'></td>
+  			<td class="table-dark">Keluar</td>
+  		</tr>
+			 <tr >
+  			<td>
+			<div class="card border-primary mb-3" style="max-width: 10rem;">
+  <div class="card-header" align='right'><button type="button" class="btn btn-danger btn-sm"">Penuh</button></div>
+  <div class="card-body">
+    <h6 class="card-title">PASAR AYAM</h6>
+    <p class="card-text"></p>
+  </div>
+  </td>
+  			<td width='50px'></td>
+  			<td  colspan='3'>	<div class="card border-primary mb-3" style="max-width: 20rem;">
+  <div class="card-header" align='right'><button type="button" class="btn btn-danger btn-sm"">Penuh</button></div>
+  <div class="card-body">
+    <h6 class="card-title" align="center">PASAR DAGING</h6>
+    <p class="card-text"></p>
+  </div></td>
+  			<td class="table-dark"></td>	
+  		
+  		</tr>
+
+  		  	</tbody>
+  </table>
+ </div>
+<body>
+</html>
